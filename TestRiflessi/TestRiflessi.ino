b63844ba977785b917fc505e75ba699169e47422
Unknown <andrea.moser@barsanti.edu.it>
2019-04-13 09:46:44 +0200

    13/4 CONSEGNA
    Bignami-Moser

--- TestRiflessi/TestRiflessi.ino
+++ TestRiflessi/TestRiflessi.ino
@@ -1,151 +1,160 @@
 
 #include <LiquidCrystal.h>
-#define BTN1 13
-#define BTN2 12
-#define BTN3 11
-#define BUZZER 6
-#define LED 7
-#define RED 2
-#define GREEN 1
+#include <LiquidCrystal.h>
+#define BTN1 7 //avvio
+#define BTN2 5 //led
+#define BTN3 3 //buzzer
+#define BUZZER 4
+#define LED 6
+#define RED 1
+#define GREEN 2
 bool firstRun;
 bool passed;
-const int rs = 10, en = 9, d4 = 8, d5 = 5, d6 = 4, d7 = 3;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
-   void setup() {
+void setup() {
   // put your setup code here, to run once:
-firstRun = true;
-passed = true;
-randomSeed(analogRead(0));
-Serial.begin(9600);
+  firstRun = true;
+  passed = true;
+  randomSeed(analogRead(0));
+  pinMode(BTN1, INPUT_PULLUP);
+  pinMode(BTN2, INPUT_PULLUP);
+  pinMode(BTN3, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
 
-pinMode(BTN1, INPUT_PULLUP);
-pinMode(BTN2, INPUT_PULLUP);
-pinMode(BTN3, INPUT_PULLUP);
-pinMode(LED, LOW);
-pinMode(BUZZER, OUTPUT);
-pinMode(RED, OUTPUT);
-pinMode(GREEN, OUTPUT);
- 
-lcd.begin(16, 2);
+  lcd.begin(16, 2);
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-if(firstRun){
-welcome();
-}
-
-testLed();
-if (passed){
+  while ( digitalRead(BTN1) == LOW) {
+    lcd.setCursor(0, 0);
+    lcd.print("Welcome!");
+    lcd.setCursor(0,1);
+    lcd.print("Press BtnRED");  
+  }
+  welcome();
+  testLed();
   testBuzzer();
+  
+  if (passed) {
+    victory();
   }
-else {failure();}
-if (passed){
-  victory();
+  else {
+    failure();
   }
-else {failure();}
+  
 }
 
-void intro(){
- firstRun = false;
- lcd.setCursor(0,0);
-  lcd.print("Press Button1 to");
- lcd.setCursor(0,1);
-  lcd.print("start countdown!");
-  while(digitalRead (BTN1) == HIGH);
+void intro() {
+  firstRun = false;
   lcd.clear();
-  for(int i = 5; i>0; i--)
+  while (digitalRead (BTN1) == LOW) {
+    lcd.setCursor(0, 0);
+    lcd.print("Press BtnRED to");
+    lcd.setCursor(0, 1);
+    lcd.print("start countdown!");
+  }
+  lcd.clear();
+  
+  for (int i = 5; i > 0; i--)
   {
-    lcd.setCursor(7,0);
+    lcd.setCursor(7, 0);
     lcd.print(String(i));
     delay(1000);
   }
-  lcd.setCursor(3,1);
+  lcd.setCursor(3, 1);
   lcd.print("Get ready!");
 }
 
-void testLed(){
-intro();
-delay(random(300,3001));
-int i1 = millis();
-digitalWrite(LED, HIGH);
-while(digitalRead (BTN2) == HIGH);
-int i2 = millis();
-digitalWrite(LED, LOW);
+void testLed() {
+  intro();
+  delay(random(300, 3001));
+  int i1 = millis();
+  digitalWrite(LED, HIGH);
+  while (digitalRead (BTN2) == LOW){
+    
+  }
+  int i2 = millis();
+  digitalWrite(LED, LOW);
 
-lcd.clear();
-lcd.setCursor(1,0);
-lcd.print(String(i2-i1)+" ms passed.");
-passed = (i2-i1 < 300);
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print(String(i2 - i1) + " ms passed.");
+  passed = (i2 - i1 < 300);
+  delay(2000);
 }
 
-void testBuzzer(){
-intro();
-delay(random(300,3001));
-int i1 = millis();
-tone(BUZZER,8000);
-int j;
-while(digitalRead (BTN3) == HIGH)
-{
-  if ((j=millis()) >= i1+6000)
-  {noTone(BUZZER);}
-}
-int i2 = millis();
-noTone(BUZZER);
+void testBuzzer() {
+  intro();
+  delay(random(300, 3001));
+  int i1 = millis();
+  tone(BUZZER,1000);
+  while (digitalRead (BTN2) == LOW){
+    
+  }
+  int i2 = millis();
+  noTone(BUZZER);
 
-lcd.clear();
-lcd.setCursor(1,0);
-lcd.print(String(i2-i1)+" ms passed.");
-passed = (i2-i1 < 300);
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print(String(i2 - i1) + " ms passed.");
+  passed = (i2 - i1 < 300);
+  delay(2000);
 }
 
-void victory(){
+void victory() {
   analogWrite(GREEN, 255);
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("You passed");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("the challenge!");
   delay(1000);
   lcd.clear();
-  lcd.print("Press Button 1");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0,0);
+  lcd.print("Press Btn_RED");
+  lcd.setCursor(0, 1);
   lcd.print("to retry!");
-  while(digitalRead (BTN1) == HIGH);
-  while(digitalRead (BTN1) == LOW);
+  while (digitalRead (BTN1) == LOW);
+  {
+    
+  }
+  analogWrite(GREEN,0);
 }
-void failure(){
+void failure() {
   analogWrite(RED, 255);
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("You failed");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("the challenge!");
   delay(1000);
   lcd.clear();
+  lcd.setCursor(0,0);
   lcd.print("Press Button 1");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("to retry!");
-  while(digitalRead (BTN1) == HIGH);
-  while(digitalRead (BTN1) == LOW);
+  while (digitalRead (BTN1) == LOW);
+  {
+    
+  }
+  analogWrite(RED,0);
 }
 
-void welcome(){
-  lcd.setCursor(4,0);
-  lcd.print("Welcome!");
-
-  lcd.setCursor(-12,1);
-  lcd.print("Button 2: Led-test");
-  lcd.print("Button 3: Buzzer-test");
-
-  while(digitalRead(BTN1) == HIGH)
-  {
-    lcd.scrollDisplayRight();
-  }
+void welcome() {
   lcd.clear();
-  /*Serial.println("");
-  Serial.println("");*/
+  lcd.setCursor(0, 0);
+  lcd.print("Button Blue: Led-test");
+  lcd.setCursor(0, 1);
+  lcd.print("Button Blue: Buzzer-test");
+
+  for (int posizione = 0; posizione <= 20; posizione++) {
+    delay(300);
+    lcd.scrollDisplayLeft();
+  }
 }
